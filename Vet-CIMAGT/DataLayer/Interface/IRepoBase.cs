e77d976e68b98dce826bf14f1057fd4af47316d8
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-26 16:48:41 -0800

    feat(RepoBase) add repositorio base y su interfaz

---
+++ Vet-CIMAGT/DataLayer/Interface/IRepoBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Vet_CIMAGT.DataLayer.Interface
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(Guid id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+         Task DeleteAsync(Guid id );
+
+    }
+}
