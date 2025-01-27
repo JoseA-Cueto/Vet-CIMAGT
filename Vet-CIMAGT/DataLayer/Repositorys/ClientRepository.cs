29054892a8e8a0d23e7ce7e8581d18f0de0af9d6
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-26 17:03:07 -0800

    feat(DataLayer) add repositorio Client y su interface

---
+++ Vet-CIMAGT/DataLayer/Repositorys/ClientRepository.cs
@@ -0,0 +1,14 @@
+﻿using Vet_CIMAGT.Core.Models;
+using Vet_CIMAGT.Data;
+using Vet_CIMAGT.DataLayer.RepoBase;
+using Vet_CIMAGT.DataLayer.Repositorys.Interface;
+
+namespace Vet_CIMAGT.DataLayer.Repositorys
+{
+    public class ClientRepository : RepositoryBase<Client>, IClientRepository
+    {
+        public ClientRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
