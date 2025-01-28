31c1cd8f46aaf4cadaa1a15cc64ec5873ccedaa2
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-27 22:55:07 -0800

    feat(CrudConsumption) agregar repository de Consumption y eliminar referencia antiguas weather forecast

---
+++ Vet-CIMAGT/DataLayer/Repositorys/ConsumptionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Vet_CIMAGT.Core.Models;
+using Vet_CIMAGT.Data;
+using Vet_CIMAGT.DataLayer.RepoBase;
+using Vet_CIMAGT.DataLayer.Repositorys.Interface;
+
+namespace Vet_CIMAGT.DataLayer.Repositorys
+{
+    public class ConsumptionRepository : RepositoryBase<Consumption>, IConsumptionRepository
+    {
+        public ConsumptionRepository(ApplicationDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
