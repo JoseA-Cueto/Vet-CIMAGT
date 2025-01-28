18bfd9fe132171d3e693828aa01fbfb5f0a0a155
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-28 00:03:19 -0800

    feat(CRUDPatient) agregar repositorio y su interfaz a la solucion

---
+++ Vet-CIMAGT/DataLayer/Repositorys/Interface/IPatientRepository.cs
@@ -0,0 +1,9 @@
+﻿using Vet_CIMAGT.Core.Models;
+using Vet_CIMAGT.DataLayer.Interface;
+
+namespace Vet_CIMAGT.DataLayer.Repositorys.Interface
+{
+    public interface IPatientRepository : IRepositoryBase<Patient>
+    {
+    }
+}
---
+++ Vet-CIMAGT/DataLayer/Repositorys/Interface/PatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using Vet_CIMAGT.Core.Models;
+using Vet_CIMAGT.Data;
+using Vet_CIMAGT.DataLayer.RepoBase;
+
+namespace Vet_CIMAGT.DataLayer.Repositorys.Interface
+{
+    public class PatientRepository : RepositoryBase<Patient>, IPatientRepository
+    {
+        public PatientRepository(ApplicationDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
